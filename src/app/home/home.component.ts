48a3c4449a309529c6c263b186a9b2476d9cb8da
Diego Andres Yave Guzmain <di.an.yave@gmail.com>
2023-07-24 18:31:44 +0200

    Event binding for click

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,4 +9,12 @@ export class HomeComponent {
   fontColor = 'blue';
   customText = "this is my custom text!";
   className = "my-class";
+  canClick = true;
+  count = 0;
+  message = "hey - ";
+
+  sayMessage() {
+    this.count++;
+    alert(this.message + this.count);
+  }
 }
